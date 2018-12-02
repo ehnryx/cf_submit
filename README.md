e7c01937ec1d5c4dad4078952da846527efaa3f9
bacali95 <nasreddine.bacali95@gmail.com>
2018-12-02 13:01:11 +0100

    add support private/public group's contests

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # cf-code-submit
 Submit code to codeforces from the command line, and other stuff (display solves for each problem, display standings). <br />
 
-In order to save the huge number of seconds needed to reach for the mouse during codeforces contests, I needed to be able to submit from the command line. There are many tools to do this for codeforces contests but I was unable to find an existing tool that allowed submissions to the codeforces gym for virtual contests, so I made this. 
+In order to save the huge number of seconds needed to reach for the mouse during codeforces contests, I needed to be able to submit from the command line. There are many tools to do this for codeforces contests but I was unable to find an existing tool that allowed submissions to the codeforces gym for virtual contests, so I made this.
 
-After being able to submit from the command line, I noticed that I was wasting too much time staring at the standings on codeforces. Now I waste less time (hopefully) by staring at the standings in the terminal. 
+After being able to submit from the command line, I noticed that I was wasting too much time staring at the standings on codeforces. Now I waste less time (hopefully) by staring at the standings in the terminal.
 
 Then I realized I would rather not navigate codeforces in my browser because looking for my mouse is a hassle, so now I can look at the number of solves for each problem in the terminal as well.
 
@@ -20,6 +20,7 @@ The handle and password are stored locally.
 # Uses
 ## Submissions
 - `con` or `gym` to set default contest or gym ID. Example: `cf con 844` or `cf gym 101482`
+- `gcon` to set default group and contest ID. Example: `cf gcon dyEemqw7jN 233642`
 - `ext` to set default file extension. Will be used when no file extension is given. (`cf submit a`)
 - `submit` to submit code. Will try to guess problem. Batch submit allowed. Example: `cf submit` or `cf submit a.cpp`
 - `--prob` or `-p` to specify problem. Example: `cf submit code.cpp -p 844a`
@@ -35,6 +36,7 @@ Examples: `cf submit code.cpp -p844a -w` <br />
 ## Print Standings
 - `standings` or `st` to look at friends' standings. Example: `cf standings`
 - `--contest` or `-c` to specify the ID of the contest to look at. Example: `cf standings -c 844`
+- `--group` to specify the ID of the group to look at. Example: `cf standings --group dyEemqw7jN`
 - `--verbose` or `-v` to print standings with more info. Example: `cf standings -v`
 - `--top` or `-t` to look top contestants. Defaults to top 50 if `-t` is not included, top 10 if `--top` is included but no number is given. Example: `cf standings -t 20`
 - `--all` or `-a` to look at all contestants instead of only friends. Example: `cf standings --all`
@@ -45,13 +47,14 @@ Examples: `cf st -v -t7` or `cf standings -c844 -v -a` <br />
 ## Print Problem Stats
 - `problems` or `pb` to look at the number of solves for each unsolved problem in a contest. Example: `cf problems`
 - `--contest` or `-c` to specify the ID of the contest. Example: `cf problems -c 844`
+- `--group` to specify the ID of the group. Example: `cf problems --group dyEemqw7jN`
 - `--verbose` or `-v` to show solved problems as well. Example: `cf problems -v`
 - `--sort` or `-s` to sort problems by: number of `solves`, or `index` (`id`). Default sort is by number of solves (you do not need to use `--sort`). Example: `cf problems -s id`
 
 Exmaples: `cf pb -v -s id` or `cf problems -c100187 -v` <br />
 
 ## Testing (Linux)
-- `test` to test code on input files. Suppose we want to test problem `a`. 
+- `test` to test code on input files. Suppose we want to test problem `a`.
 Then `cf test a` will do the job. This will call `./a < a*.in > a*.out` for each file named in the form `a*.in`. Then it will compare `a*.out` with `a*.ans` ignoring differences in white space.
 
 ## Dependencies
--- cf_submit.py
+++ cf_submit.py
@@ -5,9 +5,12 @@ import re
 import requests
 import colours
 
-""" submissions """ 
+""" submissions """
+
+
 def get_submission_data(handle):
-    req = requests.get("http://codeforces.com/api/user.status?handle={}&from=1&count=1".format(handle))
+    req = requests.get(
+        "http://codeforces.com/api/user.status?handle={}&from=1&count=1".format(handle))
     content = req.content.decode()
     js = json.loads(content)
     if "status" not in js or js["status"] != "OK":
@@ -18,44 +21,60 @@ def get_submission_data(handle):
         res["verdict"] = "IN QUEUE"
     return res["id"], res["problem"], res["verdict"], res["passedTestCount"], res["timeConsumedMillis"], res["memoryConsumedBytes"]
 
+
 """ look at last submission """
+
+
 def peek(handle):
-    id_, probject, verdict, passedTests, timeCon, memCon = get_submission_data(handle)
-    problem = str(probject["contestId"])+str(probject["index"])
+    id_, probject, verdict, passedTests, timeCon, memCon = get_submission_data(
+        handle)
+    problem = str(probject["contestId"]) + str(probject["index"])
     if verdict == "TESTING":
-        print("submission "+str(id_) + " to problem "+problem + ": "+verdict)
+        print("submission " + str(id_)
+              + " to problem " + problem + ": " + verdict)
     else:
         if verdict != "OK":
-            print("submission "+str(id_) + " to problem "+problem + ": "+verdict + " on test "+str(1+passedTests) + "\n" + "Time: "+str(timeCon)+"ms \n" + "Memory: "+str(memCon/1024)+"Kb")
+            print("submission " + str(id_) + " to problem " + problem + ": " + verdict + " on test " + str(1
+                                                                                                           + passedTests) + "\n" + "Time: " + str(timeCon) + "ms \n" + "Memory: " + str(memCon / 1024) + "Kb")
         else:
-            print("submission "+str(id_) + " to problem " + problem + ": " + verdict+"! passed all " + str(passedTests) + " tests\n" + "Time: "+str(timeCon)+"ms \n" + "Memory: "+str(memCon/1024)+"Kb")
+            print("submission " + str(id_) + " to problem " + problem + ": " + verdict + "! passed all "
+                  + str(passedTests) + " tests\n" + "Time: " + str(timeCon) + "ms \n" + "Memory: " + str(memCon / 1024) + "Kb")
+
 
 """ watch last submission """
+
+
 def watch(handle):
-    spinner = { 0: "-", 1: "/", 2: "\\" }
+    spinner = {0: "-", 1: "/", 2: "\\"}
     count = 0
     while True:
-        id_, probject, verdict, passedTests, timeCon, memCon = get_submission_data(handle)
+        id_, probject, verdict, passedTests, timeCon, memCon = get_submission_data(
+            handle)
         if "contestId" not in probject:
             probject["contestId"] = "guru"
-        problem = str(probject["contestId"])+str(probject["index"])
+        problem = str(probject["contestId"]) + str(probject["index"])
         if verdict != "TESTING" and verdict != "IN QUEUE":
-            if verdict != "OK": 
-                sys.stdout.write("\r" + "submission "+str(id_) + " to problem " + problem + ": " + verdict + " on test "+str(1+passedTests) + "\n" + "Time: "+str(timeCon)+"ms \n" + "Memory: "+str(memCon/1024)+"Kb")
+            if verdict != "OK":
+                sys.stdout.write("\r" + "submission " + str(id_) + " to problem " + problem + ": " + verdict + " on test " + str(
+                    1 + passedTests) + "\n" + "Time: " + str(timeCon) + "ms \n" + "Memory: " + str(memCon / 1024) + "Kb")
             else:
-                sys.stdout.write("\r" + "submission "+str(id_) + " to problem " + problem + ": " + verdict+"! passed all " + str(passedTests) + " tests\n" + "Time: "+str(timeCon)+"ms \n" + "Memory: "+str(memCon/1024)+"Kb")
+                sys.stdout.write("\r" + "submission " + str(id_) + " to problem " + problem + ": " + verdict + "! passed all " + str(
+                    passedTests) + " tests\n" + "Time: " + str(timeCon) + "ms \n" + "Memory: " + str(memCon / 1024) + "Kb")
             sys.stdout.flush()
             break
         else:
-            sys.stdout.write("\r" + "submission "+str(id_) + " to problem "+problem + ": "+verdict + " ....... "+spinner[count] + (' '*7))
+            sys.stdout.write("\r" + "submission " + str(id_) + " to problem "
+                             + problem + ": " + verdict + " ....... " + spinner[count] + (' ' * 7))
             sys.stdout.flush()
-            count = (count+1) % 3
+            count = (count + 1) % 3
         time.sleep(0.25)
     print('\a')
 
 
 """ submit problem """
-def submit_problem(browser, contest, lang, source, guru):
+
+
+def submit_problem(browser, group, contest, lang, source, guru):
     """ get form """
     submission = browser.get_form(class_="submit-form")
     if submission is None:
@@ -83,7 +102,7 @@ def submit_problem(browser, contest, lang, source, guru):
     elif lang == "rb":
         # Ruby 2.0.0p645
         langcode = "8"
-    elif lang == "java": 
+    elif lang == "java":
         # Java 1.8.0_112
         langcode = "36"
     elif lang == "scala":
@@ -92,7 +111,7 @@ def submit_problem(browser, contest, lang, source, guru):
         langcode = "49"
     elif lang == "php":
         langcode = "6"
-    else: 
+    else:
         print("Unknown Language")
         return False
     submission["programTypeId"] = langcode
@@ -102,7 +121,7 @@ def submit_problem(browser, contest, lang, source, guru):
         submission["submittedProblemCode"] = guru
 
     browser.submit_form(submission)
-    
+
     """ check if good """
     if (guru != -1 and browser.url[-7:] != "/status") or (guru == -1 and browser.url[-3:] != "/my"):
         print("Failed to submit code")
@@ -111,35 +130,51 @@ def submit_problem(browser, contest, lang, source, guru):
     print("Code submitted properly")
 
     """ now get time """
-    countdown_timer = browser.parsed.find_all("span", class_="contest-state-regular countdown before-contest-"+contest+"-finish")
+    countdown_timer = browser.parsed.find_all(
+        "span", class_="contest-state-regular countdown before-contest-" + contest + "-finish")
     if len(countdown_timer) > 0:
-        print(colours.bold() + "TIME LEFT: " + str(countdown_timer[0].get_text(strip=True)) + colours.reset())
-    
+        print(colours.bold() + "TIME LEFT: " +
+              str(countdown_timer[0].get_text(strip=True)) + colours.reset())
+
     return True
 
 
 """ submit problem """
-def submit(browser, handle, contest, problem, lang, source, show, guru):
+
+
+def submit(browser, handle, group, contest, problem, lang, source, show, guru):
     if guru:
         print("Submitting to acmsguru " + problem + " as " + handle)
+    elif group is not None:
+        print("Submitting to problem " + contest + problem.upper() +
+              " in group " + group + " as " + handle)
     else:
-        print("Submitting to problem " + contest + problem.upper() + " as " + handle)
+        print("Submitting to problem " + contest +
+              problem.upper() + " as " + handle)
 
     pid = -1
     if guru:
         browser.open("http://codeforces.com/problemsets/acmsguru/submit")
         pid = problem
+    elif group is not None:
+        browser.open("http://codeforces.com/group/" + group + "/contest/"
+                     + contest + "/submit/" + problem.upper())
     elif len(contest) >= 6:
-        browser.open("http://codeforces.com/gym/" + contest + "/submit/" + problem.upper())
+        browser.open("http://codeforces.com/gym/" +
+                     contest + "/submit/" + problem.upper())
     else:
-        browser.open("http://codeforces.com/contest/" + contest + "/submit/" + problem.upper())
+        browser.open("http://codeforces.com/contest/" +
+                     contest + "/submit/" + problem.upper())
 
     """ show submission """
-    if submit_problem(browser, contest, lang, source, pid) and show:
+    if submit_problem(browser, group, contest, lang, source, pid) and show:
         watch(handle)
 
+
 """ submit, possibly len(args) > 1 """
-def submit_files(browser, defaulthandle, defaultcontest, defaultprob, defext, defaultlang, args, show, guru):
+
+
+def submit_files(browser, defaulthandle, defaultgroup, defaultcontest, defaultprob, defext, defaultlang, args, show, guru):
     """ if len == 0, query for file """
     if len(args) == 0:
         args.append(raw_input("File to submit: "))
@@ -151,7 +186,7 @@ def submit_files(browser, defaulthandle, defaultcontest, defaultprob, defext, de
         if source.find('.') == -1:
             info.append(defext)
             source += "." + defext
-        
+
         """ check language """
         if defaultlang is not None:
             info[-1] = defaultlang
@@ -160,23 +195,27 @@ def submit_files(browser, defaulthandle, defaultcontest, defaultprob, defext, de
         if defaultprob is not None:
             if len(defaultprob) == 1:
                 """ letter only """
-                submit(browser, defaulthandle, defaultcontest, defaultprob, info[-1], source, show, guru)
+                submit(browser, defaulthandle, defaultgroup, defaultcontest,
+                       defaultprob, info[-1], source, show, guru)
             elif len(defaultprob) == 2:
                 """ letter + number (?) """
-                submit(browser, defaulthandle, defaultcontest, defaultprob, info[-1], source, show, guru)
+                submit(browser, defaulthandle, defaultgroup, defaultcontest,
+                       defaultprob, info[-1], source, show, guru)
             else:
                 """  parse string """
                 splitted = re.split('(\D+)', defaultprob)
                 if len(splitted) == 3 and len(splitted[1]) == 1 and len(splitted[2]) == 0:
                     """ probably a good string """
-                    submit(browser, defaulthandle, splitted[0], splitted[1], info[-1], source, show, guru)
-                else: 
+                    submit(browser, defaulthandle, defaultgroup,
+                           splitted[0], splitted[1], info[-1], source, show, guru)
+                else:
                     print("cannot understand the problem specified")
 
         elif len(info) == 2:
             if guru:
                 """ ACMSGURU """
-                submit(browser, defaulthandle, defaultcontest, info[0], info[1], source, show, guru)
+                submit(browser, defaulthandle, defaultgroup,
+                       defaultcontest, info[0], info[1], source, show, guru)
             else:
                 """ CODEFORCES """
                 """ try to parse info[0] """
@@ -185,16 +224,18 @@ def submit_files(browser, defaulthandle, defaultcontest, defaultprob, defext, de
                     info[0] = info[0][2:]
                 if len(info[0]) == 1:
                     """ only the letter, use default contest """
-                    submit(browser, defaulthandle, defaultcontest, info[0], info[1], source, show, guru)
-                else: 
+                    submit(browser, defaulthandle, defaultgroup,
+                           defaultcontest, info[0], info[1], source, show, guru)
+                else:
                     """ contest is included, so parse """
                     splitted = re.split('(\D+)', info[0])
                     if len(splitted) == 3 and len(splitted[1]) == 1 and len(splitted[2]) == 0:
                         """ probably good string ? """
-                        submit(browser, defaulthandle, splitted[0], splitted[1], info[1], source, show, guru)
+                        submit(browser, defaulthandle, defaultgroup,
+                               splitted[0], splitted[1], info[1], source, show, guru)
                     else:
-                        print("cannot parse filename, specify problem with -p or --prob")
+                        print(
+                            "cannot parse filename, specify problem with -p or --prob")
 
         else:
             print("cannot parse filename, specify problem with -p or --prob")
-
