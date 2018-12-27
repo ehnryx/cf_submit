3a07b15fcf042c3798b46edfd4c580620caf6adf
Nasreddine Bac Ali <nasreddine.bacali95@gmail.com>
2018-12-27 18:15:56 +0100

    fix bug

--- cf_hack.py
+++ cf_hack.py
@@ -28,32 +28,35 @@ def begin_hack(browser, contest, problem, generator, checker, correct_solution):
     max_pages = int(browser.find_all(class_="page-index")[-1].text)
     print(max_pages)
     print("Happy Hacking 3:)")
-    for i in range(max_pages, 0, -1):
-        browser.open("https://codeforces.com/contest/" + contest + "/status/" + problem.upper() + "/page/" + str(
-            i) + "?order=BY_PROGRAM_LENGTH_ASC")
-        submissions = browser.find_all("table", class_="status-frame-datatable")[0].find_all("tr")[1:]
-        for submission in submissions:
-            submission_id = int(submission.find("td", class_="id-cell").find("a").text)
-            language = submission.find_all("td")[4].text.strip().replace(" ", "")
-            browser.open("http://codeforces.com/contest/" + contest + "/submission/" + str(submission_id))
-            if len(browser.find_all("pre", class_="program-source")) > 0:
-                source = browser.find_all("pre", class_="program-source")[0].text
-                file_name = create_file(source, language)
-                if file_name == "":
-                    continue
-                print("\nTrying to hack a " + language + " solution - " + str(submission_id) + "...")
-                hack_process = Popen(
-                    ["/bin/bash", os.path.join(os.path.dirname(__file__), "hack_prob.sh"), generator, checker,
-                     correct_solution, file_name, language.replace(" ", "")])
-                hack_process.wait()
-                exit_code = hack_process.returncode
-                if exit_code in [0, 255]:
-                    print("Sorry, can't hack this solution x/")
-                else:
-                    test_hack_loc = os.path.join(dir_path, "workspace", "failed.txt")
-                    if os.path.isfile(test_hack_loc):
-                        print("Hope that will win 3:)")
-                        hack(browser, contest, test_hack_loc, submission_id)
+    for i in range(1, max_pages):
+        try:
+            browser.open("https://codeforces.com/contest/" + contest + "/status/" + problem.upper() + "/page/" + str(
+                i) + "?order=BY_PROGRAM_LENGTH_ASC")
+            submissions = browser.find_all("table", class_="status-frame-datatable")[0].find_all("tr")[1:]
+            for submission in submissions:
+                submission_id = int(submission.find("td", class_="id-cell").find("a").text)
+                language = submission.find_all("td")[4].text.strip().replace(" ", "")
+                browser.open("http://codeforces.com/contest/" + contest + "/submission/" + str(submission_id))
+                if len(browser.find_all("pre", class_="program-source")) > 0:
+                    source = browser.find_all("pre", class_="program-source")[0].text
+                    file_name = create_file(source, language)
+                    if file_name == "":
+                        continue
+                    print("\nTrying to hack a " + language + " solution - " + str(submission_id) + "...")
+                    hack_process = Popen(
+                        ["/bin/bash", os.path.join(os.path.dirname(__file__), "hack_prob.sh"), generator, checker,
+                         correct_solution, file_name, language.replace(" ", "")])
+                    hack_process.wait()
+                    exit_code = hack_process.returncode
+                    if exit_code in [0, 255]:
+                        print("Sorry, can't hack this solution x/")
+                    else:
+                        test_hack_loc = os.path.join(dir_path, "workspace", "failed.txt")
+                        if os.path.isfile(test_hack_loc):
+                            print("Hope that will win 3:)")
+                            hack(browser, contest, test_hack_loc, submission_id)
+        except Exception:
+            continue
 
 
 def create_file(source, language):
