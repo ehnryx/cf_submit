133f1c0e294d8a978f1c530f5329da7e88ddb6ef
Nasreddine Bac Ali <nasreddine.bacali95@gmail.com>
2018-12-27 21:34:08 +0100

    fix bug

--- cf_hack.py
+++ cf_hack.py
@@ -9,13 +9,10 @@ dir_path = os.getcwd()
 # Hack problems
 
 def hack(browser, contest, hack_test, submission_id):
-    try:
-        browser.open("https://codeforces.com/contest/" + contest + "/challenge/" + str(submission_id))
-        hack_form = browser.get_form(class_="challenge-form")
-        hack_form["testcaseFromFile"] = hack_test
-        browser.submit_form(hack_form)
-    except Exception:
-        return
+    browser.open("https://codeforces.com/contest/" + contest + "/challenge/" + str(submission_id))
+    hack_form = browser.get_form(class_="challenge-form")
+    hack_form["testcaseFromFile"] = hack_test
+    browser.submit_form(hack_form)
 
 
 def begin_hack(browser, contest, problem, generator, checker, correct_solution):
@@ -23,11 +20,16 @@ def begin_hack(browser, contest, problem, generator, checker, correct_solution):
     init_workspace_process = Popen(
         ["/bin/bash", os.path.join(os.path.dirname(__file__), "init_workspace.sh"), generator, checker,
          correct_solution])
+    tried_submissions_list = open(os.path.join(os.path.dirname(__file__), "tried_submissions"), "r")
+    list_str = tried_submissions_list.read().strip()
+    tried_submissions = list()
+    if list_str != "":
+        tried_submissions = list(map(int, list_str.split(' ')))
+    tried_submissions_list = open(os.path.join(os.path.dirname(__file__), "tried_submissions"), "a")
     init_workspace_process.wait()
     browser.open("https://codeforces.com/contest/" + contest + "/status/" + problem.upper())
     max_pages = int(browser.find_all(class_="page-index")[-1].text)
-    print(max_pages)
-    print("Happy Hacking 3:)")
+    print("Happy Hacking 3:) - max pages : "+str(max_pages))
     for i in range(1, max_pages):
         try:
             browser.open("https://codeforces.com/contest/" + contest + "/status/" + problem.upper() + "/page/" + str(
@@ -35,6 +37,11 @@ def begin_hack(browser, contest, problem, generator, checker, correct_solution):
             submissions = browser.find_all("table", class_="status-frame-datatable")[0].find_all("tr")[1:]
             for submission in submissions:
                 submission_id = int(submission.find("td", class_="id-cell").find("a").text)
+                if submission_id in tried_submissions:
+                    print(str(submission_id) + " already tried!!")
+                    continue
+                tried_submissions_list.write(str(submission_id) + " ")
+                tried_submissions_list.flush()
                 language = submission.find_all("td")[4].text.strip().replace(" ", "")
                 browser.open("http://codeforces.com/contest/" + contest + "/submission/" + str(submission_id))
                 if len(browser.find_all("pre", class_="program-source")) > 0:
@@ -42,7 +49,8 @@ def begin_hack(browser, contest, problem, generator, checker, correct_solution):
                     file_name = create_file(source, language)
                     if file_name == "":
                         continue
-                    print("\nTrying to hack a " + language + " solution - " + str(submission_id) + "...")
+                    print("\nTrying to hack a " + language + " solution - " + str(submission_id) + " on page " + str(
+                        i) + "...")
                     hack_process = Popen(
                         ["/bin/bash", os.path.join(os.path.dirname(__file__), "hack_prob.sh"), generator, checker,
                          correct_solution, file_name, language.replace(" ", "")])
