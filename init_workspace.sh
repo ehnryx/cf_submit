187cb85b34a8925e1e432fe321ea9aca97170147
Nasreddine Bac Ali <nasreddine.bacali95@gmail.com>
2018-12-28 12:00:12 +0100

    add correct codeforces compilations options

--- init_workspace.sh
+++ init_workspace.sh
@@ -20,8 +20,19 @@ WORKSPACE_DIR=workspace
 mkdir -p ${WORKSPACE_DIR} && rm -rf ${WORKSPACE_DIR}/*
 
 compile() {
-    if [[ $1 == *.c* ]]; then
-        g++ ${1} -o ${1/.*} -DONLINE_JUDGE
+    if [[ $1 == *.cpp ]]; then
+        if [[ $1 == *11* ]]; then
+            g++ ${1} -static -DONLINE_JUDGE -lm -s -x c++ -Wl,--stack=268435456 -O2 -std=c++11 -o ${1/.*}
+        elif [[ $1 == *14* ]]; then
+            g++ ${1} -static -DONLINE_JUDGE -lm -s -x c++ -Wl,--stack=268435456 -O2 -std=c++14 -o ${1/.*}
+        elif [[ $1 == *17* ]]; then
+            g++ ${1} -static -DONLINE_JUDGE -lm -s -x c++ -Wl,--stack=268435456 -O2 -std=c++17 -o ${1/.*}
+        else
+            g++ ${1} -static -DONLINE_JUDGE -lm -s -x c++ -Wl,--stack=268435456 -O2 -o ${1/.*}
+        fi
+        mv ${1/.c*} ${WORKSPACE_DIR}
+    elif [[ $1 == *.c ]]; then
+        gcc -static -fno-optimize-sibling-calls -fno-strict-aliasing -DONLINE_JUDGE -fno-asm -lm -s -Wl,--stack=268435456 -O2 -o ${1/.*}
         mv ${1/.c*} ${WORKSPACE_DIR}
     elif [[ $1 == *.java ]]; then
         cp $1 ${WORKSPACE_DIR}
