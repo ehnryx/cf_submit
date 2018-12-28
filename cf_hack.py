84a7cf2b6122dedb2e16f2a56ff6da3a5ab95574
Nasreddine Bac Ali <nasreddine.bacali95@gmail.com>
2018-12-28 12:02:32 +0100

    add small optimization to the process; add custom number of hack tests with -n or --number option

--- cf_hack.py
+++ cf_hack.py
@@ -2,20 +2,24 @@ import os
 import re
 import javalang
 from subprocess import Popen
+from robobrowser import RoboBrowser
+
+import cf_login
 
 dir_path = os.getcwd()
 
 
 # Hack problems
 
-def hack(browser, contest, hack_test, submission_id):
+def hack(contest, hack_test, submission_id):
+    browser = cf_login.login()
     browser.open("https://codeforces.com/contest/" + contest + "/challenge/" + str(submission_id))
     hack_form = browser.get_form(class_="challenge-form")
     hack_form["testcaseFromFile"] = hack_test
     browser.submit_form(hack_form)
 
 
-def begin_hack(browser, contest, problem, generator, checker, correct_solution):
+def begin_hack(contest, problem, generator, checker, correct_solution, test_number):
     global file_name
     init_workspace_process = Popen(
         ["/bin/bash", os.path.join(os.path.dirname(__file__), "init_workspace.sh"), generator, checker,
@@ -27,11 +31,14 @@ def begin_hack(browser, contest, problem, generator, checker, correct_solution):
         tried_submissions = list(map(int, list_str.split(' ')))
     tried_submissions_list = open(os.path.join(os.path.dirname(__file__), "tried_submissions"), "a")
     init_workspace_process.wait()
+
+    browser = RoboBrowser(parser="lxml")
     browser.open("https://codeforces.com/contest/" + contest + "/status/" + problem.upper())
     max_pages = int(browser.find_all(class_="page-index")[-1].text)
     print("Happy Hacking 3:) - max pages : " + str(max_pages))
     for i in range(1, max_pages):
         try:
+            browser = RoboBrowser(parser="lxml")
             browser.open("https://codeforces.com/contest/" + contest + "/status/" + problem.upper() + "/page/" + str(
                 i) + "?order=BY_PROGRAM_LENGTH_ASC")
             submissions = browser.find_all("table", class_="status-frame-datatable")[0].find_all("tr")[1:]
@@ -43,6 +50,7 @@ def begin_hack(browser, contest, problem, generator, checker, correct_solution):
                 tried_submissions_list.write(str(submission_id) + " ")
                 tried_submissions_list.flush()
                 language = submission.find_all("td")[4].text.strip().replace(" ", "")
+                browser = RoboBrowser(parser="lxml")
                 browser.open("http://codeforces.com/contest/" + contest + "/submission/" + str(submission_id))
                 if len(browser.find_all("pre", class_="program-source")) > 0:
                     source = browser.find_all("pre", class_="program-source")[0].text
@@ -53,7 +61,7 @@ def begin_hack(browser, contest, problem, generator, checker, correct_solution):
                         i) + "...")
                     hack_process = Popen(
                         ["timeout", "10", os.path.join(os.path.dirname(__file__), "hack_prob.sh"),
-                         generator, checker, correct_solution, file_name, language.replace(" ", "")])
+                         generator, checker, correct_solution, file_name, language.replace(" ", ""), str(test_number)])
                     hack_process.wait()
                     exit_code = hack_process.returncode
                     if exit_code in [0, 255]:
@@ -62,7 +70,7 @@ def begin_hack(browser, contest, problem, generator, checker, correct_solution):
                         test_hack_loc = os.path.join(dir_path, "workspace", "failed.txt")
                         if os.path.isfile(test_hack_loc):
                             print("Hope that will win 3:)")
-                            hack(browser, contest, test_hack_loc, submission_id)
+                            hack(contest, test_hack_loc, submission_id)
         except Exception:
             continue
 
--- hack_prob.sh
+++ hack_prob.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-if [[ $# -ne 5 ]]; then
-    echo "Program must be run with the following arguments: <generator-source> <checker-source> <answer-source> <for-hack-source> <for-hack-source-language>"
+if [[ $# -ne 6 ]]; then
+    echo "Program must be run with the following arguments: <generator-source> <checker-source> <answer-source>
+                <for-hack-source> <for-hack-source-language> <test-number>"
     exit -1
 fi
 
@@ -16,17 +17,27 @@ CHECKER=$2
 CORRECT_SOURCE=$3
 NOT_SURE_SOURCE=$4
 LANGUAGE=$5
+TEST_NUMBERS=$6
 
 WORKSPACE_DIR=workspace
 FAILED_TEST=failed.txt
 EXIT_CODE=0
-TEST_NUMBERS=10
 
 cd ${WORKSPACE_DIR}
 
 compile() {
-    if [[ $1 == *.c* ]]; then
-        g++ ${1} -o ${1/.*} -DONLINE_JUDGE
+    if [[ $1 == *.cpp ]]; then
+        if [[ $1 == *11* ]]; then
+            g++ ${1} -static -DONLINE_JUDGE -lm -s -x c++ -Wl,--stack=268435456 -O2 -std=c++11 -o ${1/.*}
+        elif [[ $1 == *14* ]]; then
+            g++ ${1} -static -DONLINE_JUDGE -lm -s -x c++ -Wl,--stack=268435456 -O2 -std=c++14 -o ${1/.*}
+        elif [[ $1 == *17* ]]; then
+            g++ ${1} -static -DONLINE_JUDGE -lm -s -x c++ -Wl,--stack=268435456 -O2 -std=c++17 -o ${1/.*}
+        else
+            g++ ${1} -static -DONLINE_JUDGE -lm -s -x c++ -Wl,--stack=268435456 -O2 -o ${1/.*}
+        fi
+    elif [[ $1 == *.c ]]; then
+        gcc -static -fno-optimize-sibling-calls -fno-strict-aliasing -DONLINE_JUDGE -fno-asm -lm -s -Wl,--stack=268435456 -O2 -o ${1/.*}
     elif [[ $1 == *.java ]]; then
         javac $1
     elif [[ $1 == *.py ]]; then
@@ -49,11 +60,11 @@ execute() {
         fi
     elif [[ $1 == *.java ]]; then
         if [[ $# -eq 2 ]]; then
-            java -DONLINE_JUDGE=true ${1/.*} $2
+            java -Xmx512M -Xss64M -DONLINE_JUDGE=true -Duser.language=en -Duser.region=US -Duser.variant=US ${1/.*} $2
         elif [[ $# -eq 3 ]]; then
-            java -DONLINE_JUDGE=true ${1/.*} < $2 > $3
+            java -Xmx512M -Xss64M -DONLINE_JUDGE=true -Duser.language=en -Duser.region=US -Duser.variant=US ${1/.*} < $2 > $3
         else
-            java -DONLINE_JUDGE=true ${1/.*} ${@:2}
+            java -Xmx512M -Xss64M -DONLINE_JUDGE=true -Duser.language=en -Duser.region=US -Duser.variant=US ${1/.*} ${@:2}
         fi
     elif [[ $1 == *.py ]]; then
         if [[ ${LANGUAGE} == *2* ]]; then
@@ -87,8 +98,12 @@ execute ${GENERATOR} ${TEST_NUMBERS} &> /dev/null
 for i in test*.in; do
     if [[ -f ${i} ]]; then
         execute ${NOT_SURE_SOURCE} ${i} ${i/.in/.out} &> /dev/null
-        if [[ $? -ne 0 ]]; then
-            echo "Execution error - code $? !!"
+        EXIT_CODE=$?
+        if [[ ${EXIT_CODE} -ne 0 ]]; then
+            printf "${GREEN}"
+            echo "Execution error - code $EXIT_CODE !!"
+            echo -en "\007"
+            clean
             exit 1
         fi
         execute ${CORRECT_SOURCE}  ${i} ${i/.in/.ans} &> /dev/null
@@ -98,6 +113,7 @@ for i in test*.in; do
             printf "${GREEN}"
             cat ${i} > ${FAILED_TEST}
             echo "Can be Hacked, trying..."
+	    echo -en "\007"
             clean
             exit ${EXIT_CODE}
         fi
