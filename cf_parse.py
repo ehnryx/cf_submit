84abcc9a08cac1d54692bda4df53aa1277d79718
bacali95 <nasreddine.bacali95@gmail.com>
2019-01-25 17:53:58 +0100

    add parse samples and test source code args

---
+++ cf_parse.py
@@ -0,0 +1,42 @@
+import os
+from subprocess import Popen
+from robobrowser import RoboBrowser
+
+from colors import colors
+
+
+def parse(contest, problem):
+    Popen(["rm", "-rf", "files"]).wait()
+    Popen(["mkdir", "-p", "files"]).wait()
+    print("%sImporting samples of problem %s...%s" %
+          (colors.WARNING, str(contest + problem), colors.ENDC))
+    j = 0
+    try:
+        browser = RoboBrowser(parser="lxml")
+        browser.open("https://codeforces.com/problemset/problem/%s/%s"
+                     % (contest, problem))
+        page = browser.parsed
+        sample_test = page.find("div", class_="sample-test")
+        inputs = sample_test.find_all("div", class_="input")
+        for i in inputs:
+            input_text = i.find("pre")
+            create_file(str(input_text.text).strip(), "test%d.in" % (j))
+            j += 1
+        outputs = sample_test.find_all("div", class_="output")
+        j = 0
+        for i in outputs:
+            output_text = i.find("pre")
+            create_file(str(output_text.text).strip(), "test%d.ans" % (j))
+            j += 1
+    except Exception:
+        print("%sPlease check you internet connection !!%s" %
+              (colors.FAIL, colors.ENDC))
+        return
+    print("%s%d samples imported successfully!%s" %
+          (colors.OKGREEN, j, colors.ENDC))
+
+
+def create_file(source, file_name):
+    for_hak_source = open(os.path.join("files", file_name), "w")
+    for_hak_source.write(source)
+    for_hak_source.close()
---
+++ cf_test.py
@@ -0,0 +1,76 @@
+import os
+import glob
+from subprocess import Popen
+
+from colors import colors
+
+
+def test(source, lang):
+    info = source.split('.')
+    if lang is None:
+        lang = info[-1]
+    if lang == "py":
+        print("%sPlease specify the version of python : py2 or py3%s" %
+              (colors.FAIL, colors.ENDC))
+        return
+    print("%sCompiling your %s solution..." %
+          (colors.OKBLUE, lang))
+    comp(source, lang, info)
+
+    print("Executing...%s" % colors.ENDC)
+    i = 1
+    success = 0
+    failed = 0
+    for file_name in glob.iglob('files/test*.in'):
+        with open(file_name, "r") as input_file:
+            with open(file_name.replace(".in", ".out"), "w") as output_file:
+                print("%s#################### TEST %d ####################%s\n" %
+                      (colors.WARNING, i, colors.ENDC))
+                i += 1
+                execute(source, lang, info, input_file, output_file)
+                proccess = Popen(["cf_checker", file_name,
+                                  file_name.replace(".in", ".out"), file_name.replace(".in", ".ans")])
+                proccess.wait(timeout=5)
+                exit_code = proccess.returncode
+                print("\n%sInput:%s" % (colors.OKBLUE, colors.ENDC))
+                print(open(file_name, 'r').read(), "\n")
+                if exit_code == 0:
+                    success += 1
+                else:
+                    print("%sExpected:%s" % (colors.OKBLUE, colors.ENDC))
+                    print(
+                        str(open(file_name.replace(".in", ".ans"), 'r').read()).strip(), "\n")
+                    failed += 1
+                print("%sOutput:%s" % (colors.OKBLUE, colors.ENDC))
+                print(
+                    str(open(file_name.replace(".in", ".out"), 'r').read()).strip(), "\n")
+
+    print("%s################################################%s\n" %
+          (colors.WARNING, colors.ENDC))
+    print("%sSuccess: %d, %sFailed: %d%s" %
+          (colors.OKGREEN, success, colors.FAIL, failed, colors.ENDC))
+
+
+def execute(source, lang, info, input_file, output_file):
+    if lang == "cpp" or lang == "c":
+        cmd = "./%s" % (info[0])
+    elif lang == "java":
+        cmd = "java %s" % (info[0])
+    elif lang == "py2":
+        cmd = "python2 %s" % (source)
+    elif lang == "py3":
+        cmd = "python3 %s" % (source)
+    else:
+        print("Sorry language not supported!")
+        exit(-1)
+    Popen(cmd, stdin=input_file,
+          stdout=output_file, shell=True).wait()
+
+
+def comp(source, lang, info):
+    if lang == "cpp":
+        Popen("g++ %s -o %s" % (source, info[0]), shell=True).wait()
+    elif lang == "c":
+        Popen("gcc %s -o %s" % (source, info[0]), shell=True).wait()
+    elif lang == "java":
+        Popen("javac %s" % (source), shell=True).wait()
